7ed9967eff274bc44c511bf7d25d576d8644b10a
Brooklyn P <165840788+broooklynp@users.noreply.github.com>
2025-01-30 21:58:20 -0800

    added javascript for projects

--- global.js
+++ global.js
@@ -72,3 +72,39 @@ form?.addEventListener('submit', function (event) {
     }
     location.href = email_url;
 });
+
+export async function fetchJSON(url) {
+    try {
+        // Fetch the JSON file from the given URL
+        const response = await fetch(url);
+        console.log(response)
+        if (!response.ok) {
+            throw new Error(`Failed to fetch projects: ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error('Error fetching or parsing JSON data:', error);
+    }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+    const titleElement = document.querySelector('.projects-title');
+    if (titleElement) {
+        titleElement.textContent = `${project.length} Projects`;
+    }
+    containerElement.innerHTML = '';
+    project.forEach(project => {
+        const article = document.createElement('article');
+        article.innerHTML = `
+        <${headingLevel}>${project.title}</${headingLevel}>
+        <img src="${project.image}" alt="${project.title}">
+        <p>${project.description}</p>`;
+        containerElement.appendChild(article);
+    });
+}
+
+export async function fetchGitHubData(username) {
+    return fetchJSON(`https://api.github.com/users/${username}`);
+}
